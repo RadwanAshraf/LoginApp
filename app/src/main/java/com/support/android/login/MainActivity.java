2815d1d3c0362db60026be41d26c16a214500f7b
RadwanFCIS <radwan.ashraf.fcis@gmail.com>
2018-04-06 19:58:17 +0200

    hands on solution

--- app/src/main/java/com/support/android/login/MainActivity.java
+++ app/src/main/java/com/support/android/login/MainActivity.java
@@ -14,62 +14,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mRegistration;
-    EditText email;
-    EditText pass;
-    static final String KEY_EMAIL = "username";
-    static final String KEY_PASSWORD = "password";
-    Button mSignIn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        Intent intent = new Intent(getApplicationContext(),Reg_Form.class);
-        startActivity(intent);
+        setContentView(R.layout.registration_form);
 
-        UID();
+}
 
-/*        mRegistration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Reg_Form.class);
-                startActivity(intent);
-            }
-        });
-        mSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences myPrefs = getSharedPreferences("mySharedPref", Context.MODE_PRIVATE);
-                String mail = myPrefs.getString("email","None");
-                String password = myPrefs.getString("passWord","");
-                String emailText = email.getText().toString();
-                String passtext = pass.getText().toString();
-                if ( emailText.equals(mail) && passtext.equals(password)) {
-
-                    Intent intent = new Intent(getApplicationContext(), AfterLogIn.class);
-                    startActivity(intent);
-
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),
-                            "Wrong Email or Password Please Check again" ,
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-*/
-    }
-
-    public  void UID () {
-
-        email = findViewById(R.id.your_email_address);
-        pass = findViewById(R.id.create_new_password);
-        mRegistration = findViewById(R.id.registration);
-        mSignIn = findViewById(R.id.signin);
-
-    }
-
-    public void SP (){
-
-    }
 }
